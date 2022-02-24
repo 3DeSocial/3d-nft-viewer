ef494e961400375f1553e3b8a3b97779fb15bf22
Andrew van Duivenbode <andrew@creativetransmissions.com>
2022-02-24 16:18:26 +0000

    WIP Three rendering

--- public/js/3dviewer.js
+++ public/js/3dviewer.js
@@ -1,5 +1,11 @@
  (function() { 
 
+	var container;
+
+	var camera, controls, scene, renderer;
+
+	var cameraFov = 60;
+
  	updateUI = (el, modelUrl) => {
 		//console.log('showing button for '+modelUrl);
 		var a = document.createElement('a');
@@ -15,35 +21,94 @@
  	}
 
  	addClickListener = (el, modelUrl) => {
-		console.log('adding listener for '+modelUrl);
+		//console.log('adding listener for '+modelUrl);
 		el.addEventListener("click", (e)=>{
 			e.preventDefault();
 			e.stopPropagation();
 			console.log('init three for: '+modelUrl);
+			initThree(modelUrl);
 		});		
  	}
-
- 	initViewer = () => {
- 		let modelUrl = this.url;
-		console.log('Load Three JS Environment for '+modelUrl);
- 	}
- 	
+	
  	initModel = (el) => {
  		const that = this;
- 		let url = '/nfts/a183b36867a953166bef9e3f5f461bb3b0c772da50093acf614331313fee2a8d'
+ 		let nftPostHash = 'a183b36867a953166bef9e3f5f461bb3b0c772da50093acf614331313fee2a8d';
+ 		let url = '/nfts/'+nftPostHash;
  		fetch(url)
  		.then(response => response.json())
  		.then((data)=>{	
 
  			if(data !== undefined){
- 				console.log(data);
- 				let link = this.updateUI(el, data.modelUrl);
- 				this.addClickListener(link, data.modelUrl);
+ 				let fullUrl = '/models/'+nftPostHash+data.modelUrl;
+ 				let link = this.updateUI(el, fullUrl);
+ 				this.addClickListener(link, fullUrl);
  			};
 
  		}).catch(err => alert(err));
 
  	}
+
+	function initThree(modelUrl) {
+
+	  renderer = new THREE.WebGLRenderer( { antialias: false } );
+	  renderer.setClearColor( 0xc3ffaa, 1.0 );
+	  renderer.setSize( window.innerWidth, window.innerHeight );
+
+	  container = document.getElementById( 'container' );
+	  container.innerHTML = '';
+	  container.appendChild( renderer.domElement );
+
+	  camera = new THREE.PerspectiveCamera( cameraFov, window.innerWidth / window.innerHeight, 0.1, 1000 );
+	  camera.aspect = container.innerWidth / container.innerHeight;
+	  camera.updateProjectionMatrix();
+
+	  const gltfLoader = new THREE.GLTFLoader();
+	  const url = modelUrl;
+console.log('gltfLoader: '+url);
+	  gltfLoader.load(url, (gltf) => {
+
+	        console.log(gltf);
+
+	    scene = new THREE.Scene();
+	    const root = gltf.scene;
+	    renderer.render( root, camera );
+
+
+	    var box = new THREE.Box3().setFromObject( root.children[0]);
+	    var sizeX = box.getSize().x;
+	    var sizeY = box.getSize().y;
+	    var sizeZ = box.getSize().z;
+	    console.log(sizeX, sizeY, sizeZ);
+	    console.log(box.getSize());
+	    let objectSize = Math.max( sizeX, sizeY );
+	    console.log('max size: ',objectSize);
+	    // Calculate the camera distance
+
+	    var aspect = window.innerWidth / window.innerHeight;
+	    var fov = cameraFov * ( Math.PI / 180 );
+
+	      console.log('fov: ',fov);
+	    var distance = Math.abs( objectSize / Math.sin( fov / 2 ) );
+	       console.log('distance: ',distance);     
+	    var cameraPosition = new THREE.Vector3(
+	    0,
+	    scene.children[0].position.y + Math.abs( objectSize / Math.sin( fov / 2 ) ),
+	    0);
+	    console.log(cameraPosition);
+	    //camera.position.set(cameraPosition);
+	    camera.lookAt( new THREE.Vector3( 0, 0, 0 ) );
+	    renderer.render( scene, camera );
+
+	  });
+
+	}
+
+	function animate() {
+
+	  requestAnimationFrame( animate );
+	  render();
+
+	} 	
 
  	let nfts = Array.from(document.getElementsByClassName('nft-viewer'));
 
